2182bc2f3cd53d580c78bc51d7801767d78324e2
inhwan.jung <inhwan.jung@gmail.com>
2022-06-03 13:56:01 +0900

    Update telegraf-relay.py

--- kafka/telegraf-relay.py
+++ kafka/telegraf-relay.py
@@ -5,10 +5,11 @@ import json
 KR1_TOPIC = "telegraf-kr1-metric"
 KR2_TOPIC = "telegraf-kr2-metric"
 
-brokers = ["10.11.76.177:9092", "10.11.76.177:9093", "10.11.76.177:9094"]
+kr1_brokers = ["10.11.76.177:9092", "10.11.76.177:9093", "10.11.76.177:9094"]
+kr2_brokers = ["172.17.38.194:9092", "172.17.38.194:9093", "172.17.38.194:9094"]
 
-consumer = KafkaConsumer(KR1_TOPIC, bootstrap_servers=brokers)
-producer = KafkaProducer(bootstrap_servers=brokers)
+consumer = KafkaConsumer(KR1_TOPIC, bootstrap_servers=kr1_brokers)
+producer = KafkaProducer(bootstrap_servers=kr2_brokers)
 
 for message in consumer:
   msg = json.loads(message.value.decode())
